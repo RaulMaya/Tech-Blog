b0d816c747b7e444504db4d2dae800d9398e7e5f
Raul <raulmayas20@gmail.com>
2023-04-01 23:04:03 -0600

    Creating sql schema and index routes

---
+++ controllers/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const homeRoutes = require("./home-routes");
+const apiRoutes = require("./api");
+
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+router.get("*", function (req, res) {
+  res.render("404", { title: "Page Not Found" });
+});
+
+module.exports = router;
