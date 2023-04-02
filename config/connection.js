8cd0c64432d6a92a1c7c5b2841506e125c3919b8
Raul <raulmayas20@gmail.com>
2023-04-01 22:54:01 -0600

    Server configuration done

--- config/connection.js
+++ config/connection.js
@@ -4,18 +4,18 @@ require("dotenv").confi();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect:'mysql',
-            port: 3006
-        }
-    )
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3006,
+    }
+  );
 }
 
-module.exports = sequelize
+module.exports = sequelize;
