c3ac344efa2e5db37d39e2f7981514fab77eba06
Raul <raulmayas20@gmail.com>
2023-04-17 19:40:13 -0600

    Reading and writing posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,6 @@ router.post("/", async (req, res) => {
   console.log(req.session.userId)
   try {
     const userId = req.session.userId
-    console.log("Burning Love", req.session.userId)
     const { postTitle, postText } = req.body;
     if (!req.body || !userId) {
       return res.status(400).json({ error: "Missing values" });
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
 const withAuth = require("../utils/withAuth");
+const { User, Post } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("index", { loggedIn: req.session.loggedIn, title:"TTS: The Tech Shore" });
+    const dbPosts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["firstname", "lastname", "username"],
+        },
+      ],
+    });
+
+    let posts = dbPosts.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("index", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      userid: req.session.userId,
+      title: "TTS: The Tech Shore",
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -12,12 +29,39 @@ router.get("/", async (req, res) => {
 
 router.get("/write", withAuth, async (req, res) => {
   try {
-    res.render("write", { loggedIn: req.session.loggedIn, title:"TTS: The Tech Shore" });
+    res.render("write", {
+      loggedIn: req.session.loggedIn,
+      title: "TTS: The Tech Shore",
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+router.get("/read", async (req, res) => {
+  try {
+    const dbPosts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["firstname", "lastname", "username"],
+        },
+      ],
+    });
+
+    let posts = dbPosts.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("read", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      userid: req.session.userId,
+      title: "TTS: The Tech Shore",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
