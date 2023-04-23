9b63d8d9fcab8438d2b4eaa0fef1f458960d310f
Raul <raulmayas20@gmail.com>
2023-04-23 15:27:41 -0600

    Love

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       document.location.replace("/");
-      window.location.reload();
+      window.location.reload(true);
     } else {
       alert("Failed to log in.");
     }
